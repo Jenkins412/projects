a331ba46b7680ec1859ebb3ac1cffc1b25bc3c5e
Jenkins412 <97547314+Jenkins412@users.noreply.github.com>
2022-12-27 19:41:08 -0500

    Update Create Your Own Bracket.py

--- Create Your Own Bracket.py
+++ Create Your Own Bracket.py
@@ -2,7 +2,7 @@ import random # Simple project that takes user input to seed a bracket for a des
 
 players = input("How many people are participating in your tournament? ")
 
-def make_bracket(list_of_players):
+def seed_bracket(list_of_players):
     list_of_seeds = []    
     x = random.randint(0, len(list_of_players)-1)
     for e in range(0,len(list_of_players)):
@@ -10,14 +10,20 @@ def make_bracket(list_of_players):
         while x in list_of_seeds:
             x = random.randint(0,len(list_of_players)-1)
         list_of_seeds = list_of_seeds + [x]
-    print(list_of_seeds)
-    seeds_in_order = []
-    for e in range(len(list_of_players)):
-        for f in range(len(list_of_players)):
+    seed_in_order = {}
+    updated_seeds = {} # this stores the inputs into a dictionary in case I wanted to take this project further by linking it to the front end
+    for e in range(0,len(list_of_players)): 
+        for f in range(0,len(list_of_players)):
             if e == list_of_seeds[f]:
-                seeds_in_order = seeds_in_order + [str(list_of_players[f])+": " + str(list_of_seeds[f]+1)]
+                updated_seeds.update({str(list_of_seeds[f]+1):str(list_of_players[f])})
+    seeds_in_order = [] # this part prints them in a list
+    for e in range(0,len(list_of_players)):
+        for f in range(0,len(list_of_players)):
+            if e == list_of_seeds[f]:
+                seeds_in_order = seeds_in_order + [str(list_of_seeds[f]+1)+": " + str(list_of_players[f])]
     for e in (seeds_in_order):
         print(e)
+    makethisbracket(list_of_players,updated_seeds,seeds_in_order)
 
 def bracket(players):
     x = 1
@@ -28,7 +34,6 @@ def bracket(players):
         player_count = player_count - 1
         x = x + 1
         list_of_players = list_of_players + [playername]
-    print(list_of_players)
-    make_bracket(list_of_players)
+    seed_bracket(list_of_players)
 
 bracket(players)
